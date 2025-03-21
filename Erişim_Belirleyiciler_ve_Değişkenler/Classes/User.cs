a13a8ed2e93e2301a26b624f501413dd6df37e28
burak uruk <burakurruk@gmail.com>
2025-03-21 17:07:17 +0200

    Proje dosyası ekle.

---
+++ Erişim_Belirleyiciler_ve_Değişkenler/Classes/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erişim_Belirleyiciler_ve_Değişkenler.Classes
+{
+    class User
+    {
+        private string adSoyad;
+        private int yas;
+        public string Email { get; set; }
+
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string isim)
+        {
+            adSoyad = isim;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int kullaniciYasi)
+        {
+            if (kullaniciYasi > 0) 
+            {
+                yas = kullaniciYasi;
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz yaş girdiniz.");
+            }
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Ad Soyad: "+ adSoyad);
+            Console.WriteLine("Yaş: "+ yas);
+            Console.WriteLine("Email: "+ Email);
+        }
+    }
+}
---
+++ Erişim_Belirleyiciler_ve_Değişkenler/Program.cs
@@ -0,0 +1,15 @@
+﻿using Erişim_Belirleyiciler_ve_Değişkenler.Classes;
+
+public class Program
+{
+    static void Main()
+    {
+        User kullanici = new User();
+
+        kullanici.SetAdSoyad("Ahmetburak Uruk");
+        kullanici.SetYas(21);
+        kullanici.Email = "Burakurukb@gmail.com";
+
+        kullanici.BilgileriGoster();
+    }
+}
